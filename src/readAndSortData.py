24a8852a665a5c1d5a6bce1905a95085aa6f77cc
Geoffrey Nguyen <geoffrey.nguyen@email.ucr.edu>
2021-06-21 22:21:10 -0700

    Sorted data into array for all teams

--- src/readAndSortData.py
+++ src/readAndSortData.py
@@ -13,7 +13,7 @@ total_array = []
 
 # gets each team and its stats
 print(Lines[:10])
-for line in Lines[:10]:
+for line in Lines:
     team_name, wins, losses, ties, win_percent, streak, rounds_won, rounds_lost = "", "", "", "", "", "", "", ""
     # start from back, and get each metric
     # print(line)
@@ -31,31 +31,39 @@ for line in Lines[:10]:
         i -= 1
     current_array.insert(0, rounds_won)
 
-
+    i -= 1
+    while line[i] != " ":
+        streak = line[i] + streak
+        i -= 1
+    current_array.insert(0, streak)
+    i -= 1
+    while line[i] != " ":
+        win_percent = line[i] + win_percent
+        i -= 1
+    current_array.insert(0, win_percent)
+    i -= 1
+    while line[i] != " ":
+        ties = line[i] + ties
+        i -= 1
+    current_array.insert(0, ties)
+    i -= 1
+    while line[i] != " ":
+        losses = line[i] + losses
+        i -= 1
+    current_array.insert(0, losses)
+    i -= 1
+    while line[i] != " ":
+        wins = line[i] + wins
+        i -= 1
+    current_array.insert(0, wins)
+    i -= 1
+    while line[i] != "." and i > 1:
+        team_name = line[i] + team_name
+        i -= 1
+    team_name = team_name[1:]
+    current_array.insert(0, team_name)
     
-    # i -= 1
-    # while line[i] != " ":
-    #     pass
-    # i -= 1
-    # while line[i] != " ":
-    #     pass
-    # i -= 1
-    # while line[i] != " ":
-    #     ties = line[i] + ties
-    #     i -= 1
-    # current_array.insert(0, ties)
-    # i -= 1
-    # while line[i] != " ":
-    #     losses = line[i] + losses
-    #     i -= 1
-    # current_array.insert(0, losses)
-    # i -= 1
-    # while line[i] != " ":
-    #     wins = line[i] + wins
-    #     i -= 1
-    # current_array.insert(0, wins)
-    # i -= 1
-    # print(current_array)
+    print(current_array)
 
     total_array.append(current_array)
 
