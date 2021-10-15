71d64130d335091a5c585f785cbc42e981638fc2
Geoffrey Nguyen <gnguy011@ucr.edu>
2021-10-15 14:58:32 -0700

    Updated team search, informs teams their record and if they are in standings for playoffs

--- src/FindTeam.js
+++ src/FindTeam.js
@@ -5,17 +5,43 @@ class FindTeam extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            teamName: ""
+            teamName: "",
+            array: [],
+            isInPlayoffs: "",
+            wins: 0,
+            losses: 0
         }
         this.handleChange = this.handleChange.bind(this)
+        this.handleClick = this.handleClick.bind(this)
     }
 
     handleChange(event) {
         this.setState({ teamName: event.target.value })
     }
 
-    render() {
-        var array = [];
+    handleClick() {
+        for (var i = 0; i < this.state.array.length; i++) {
+            if (this.state.teamName === this.state.array[i][1]) {
+                if (i > 127) {
+                    this.setState ({ 
+                        wins: this.state.array[i][2],
+                        losses: this.state.array[i][3],
+                        isInPlayoffs: "You are currently not in the standing for playoffs. Your record is " + this.state.array[i][2] + "-" + this.state.array[i][3] + "."
+                    });
+                }
+                else {
+                    this.setState({ 
+                        wins: this.state.array[i][2],
+                        losses: this.state.array[i][3],
+                        isInPlayoffs: "You are currently in the standing for playoffs. Your record is " + this.state.array[i][2] + "-" + this.state.array[i][3] + "."
+                    });
+                }
+            }
+        }
+    }
+
+    componentDidMount() {
+        var tempArray = []
         fetch(text)
         .then(r => r.text())
         .then(text1 => {
@@ -31,10 +57,10 @@ class FindTeam extends React.Component {
                     team[3] = parseInt(team[3]);
                     team[4] = parseInt(team[4]);
                     team[5] = parseInt(team[5]);
-                    array.push(team);
+                    tempArray.push(team);
                 }
             }
-            array = array.sort((a, b) => {
+            tempArray = tempArray.sort((a, b) => {
                 if (a[2] === b[2]) {
                     if (a[3] === b[3]) {
                         if (a[4] === b[4]) {
@@ -46,14 +72,39 @@ class FindTeam extends React.Component {
                 }
                 return b[2] - a[2]
             });
+            
+            var teams = document.getElementById("teams");
+            // console.log(tempArray.length);
+            for (var i = 0; i < tempArray.length; i++) {
+                var option = document.createElement("option");
+                option.value = tempArray[i][1];
+                teams.appendChild(option);
+                // console.log(tempArray[i]);
+            }
+            this.setState({
+                array: tempArray
+            });
         })
+    }
+
+    render() {
         return (
             <div class="container pt-5">
+                <h3>Find your team here: </h3>
                 <input 
                 type="text" 
                 className="form-control"
                 onChange={this.handleChange} 
+                list="teams"
                 />
+                <datalist id="teams"></datalist>
+                <button 
+                className="btn btn-primary mt-3"
+                onClick={this.handleClick}
+                >
+                    Check playoffs status
+                </button>
+                <p className="mt-2" id="isInPlayoffs">{this.state.isInPlayoffs}</p>
             </div>
         )
     }
